110a1ec2cf4c7c66731228174aeae0fa6c129075
MUEDSA <7676275+muedsa@users.noreply.github.com>
2025-07-24 17:33:43 +0800

    release: 0.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.muedsa.tvbox.ha"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 5
+        versionName = "0.0.5"
     }
 
     signingConfigs {
