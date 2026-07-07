1004b3753850d3bb22fe539b2095515a882e0766
MUEDSA <7676275+muedsa@users.noreply.github.com>
2026-07-07 17:28:04 +0800

    release: 0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.muedsa.tvbox.ha"
         minSdk = 24
         targetSdk = 37
-        versionCode = 10
-        versionName = "0.0.10"
+        versionCode = 11
+        versionName = "0.1.0"
     }
 
     signingConfigs {
